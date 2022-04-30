4b16677ef0078f71b19b19bd8986f27378bcd73e
nik <n.moiseienko@selectoglobal.com>
2022-05-01 01:42:44 +0300

    add apps/order-book to submodules

--- apps/order-book/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { OrderBookModule } from './order-book.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(OrderBookModule);
-  await app.listen(3000);
-}
-bootstrap();
